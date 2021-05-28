1441763be9dcf9e16a84017da983f5e6563004e6
unknown <terencetan@hotmail.ca>
2021-05-27 21:47:29 -0400

    added menu page as well as new matrials/routes for the menu

--- controllers/pages.js
+++ controllers/pages.js
@@ -2,8 +2,12 @@ function landingPage(req, res, next) {
     res.render('index', { title: 'Easy Money Sniper' });
   }
 
+function menuPage(req, res, next) {
+    res.render('menu', );
+  }
 
 module.exports = {
     landingPage,
+    menuPage,
   };
   
--- routes/menu.js
+++ routes/menu.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const pagesCtrl = require('../controllers/pages');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', pagesCtrl.menuPage);
 
 module.exports = router;
